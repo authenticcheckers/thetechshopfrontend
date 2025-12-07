b6ffce000ee4367eeb8f3f760f6af27494c977e4
authenticcheckers <authenticcheckers@gmail.com>
2025-12-07 01:06:48 +0000

    Delete components/components directory

--- components/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-secondary text-neutral-400 p-5 text-center">
-      &copy; 2025 The Tech Shop. All rights reserved.
-    </footer>
-  );
-}

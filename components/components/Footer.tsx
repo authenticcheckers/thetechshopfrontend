d8a45f8ee5e177acb41b332af55f8ab8187c1a51
authenticcheckers <authenticcheckers@gmail.com>
2025-12-07 01:06:08 +0000

    Create Footer.tsx

---
+++ components/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-secondary text-neutral-400 p-5 text-center">
+      &copy; 2025 The Tech Shop. All rights reserved.
+    </footer>
+  );
+}

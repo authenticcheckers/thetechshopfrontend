d952ecd849a725951f726246e60d15917f0300d2
authenticcheckers <authenticcheckers@gmail.com>
2025-12-07 01:08:01 +0000

    Create CartItem.tsx

---
+++ components/CartItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useCart } from '../context/CartContext';
+
+export default function CartItem({ item }: any) {
+  const { removeFromCart } = useCart();
+  return (
+    <div className="flex justify-between items-center bg-neutral-800 p-3 rounded-lg mb-2">
+      <span>{item.name}</span>
+      <span>{item.price} GHS</span>
+      <button onClick={() => removeFromCart(item.id)} className="text-red-500">Remove</button>
+    </div>
+  );
+}

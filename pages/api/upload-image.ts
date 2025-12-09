a0dc1e9df1a322581083b231c35f680ee1a4e3b3
authenticcheckers <authenticcheckers@gmail.com>
2025-12-09 02:18:23 +0000

    Update upload-image.ts

--- pages/api/upload-image.ts
+++ pages/api/upload-image.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,

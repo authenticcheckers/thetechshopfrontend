51afa13ed95510f28c63435b133a2049c12d78bf
authenticcheckers <authenticcheckers@gmail.com>
2025-12-09 14:01:35 +0000

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -2,16 +2,22 @@ import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 
+/**
+ * PRODUCTS
+ */
 export const getProducts = async () => {
   const res = await axios.get(`${API_URL}/products/list`);
   return res.data.products;
 };
 
-export const getProductById = async (id: string) => {
+export const getProductById = async (id: number | string) => {
   const res = await axios.get(`${API_URL}/products/${id}`);
-  return res.data;
+  return res.data.product;
 };
 
+/**
+ * ORDERS
+ */
 export const createOrder = async (orderData: any) => {
   const res = await axios.post(`${API_URL}/orders/create`, orderData);
   return res.data;

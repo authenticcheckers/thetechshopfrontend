e31231400a4f5602db2f8e26bea8d0d9f340f810
authenticcheckers <authenticcheckers@gmail.com>
2025-12-10 17:28:02 +0000

    Update CartItem.tsx

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { useCart } from '../hooks/useCart'; // Correct import
+import { useCart } from "../hooks/useCart";
+import { CartItem as CartItemType } from "../types/CartItem";
 
-export default function CartItem({ item }: any) {
+export default function CartItem({ item }: { item: CartItemType }) {
   const { removeFromCart } = useCart();
+
   return (
     <div className="flex justify-between items-center bg-neutral-800 p-3 rounded-lg mb-2">
       <span>{item.name}</span>
-      <span>{item.price} GHS</span>
-      <button onClick={() => removeFromCart(item.id)} className="text-red-500">Remove</button>
+      <button
+        onClick={() => removeFromCart(item.id)}
+        className="bg-red-600 px-2 py-1 rounded text-white"
+      >
+        Remove
+      </button>
     </div>
   );
 }

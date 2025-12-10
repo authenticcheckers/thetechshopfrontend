cff14f45dd820274a86fe8224b566eabecc3aca5
authenticcheckers <authenticcheckers@gmail.com>
2025-12-10 17:22:01 +0000

    Update useCart.ts

--- hooks/useCart.ts
+++ hooks/useCart.ts
@@ -1,6 +1,16 @@
 import { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 
+import { CartItem } from '../types/CartItem';
+
+export const useCart = () => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+  const addToCart = (item: CartItem) => {
+    setCart((prev) => [...prev, item]);
+  };
+  return { cart, addToCart };
+};
+
 export const useCart = () => {
   const context = useContext(CartContext);
   if (!context) throw new Error('useCart must be used within a CartProvider');
